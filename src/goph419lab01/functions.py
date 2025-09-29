7f3ee8d23c3eefb60b82daed36eec744372b21b0
OwenDeMeyer <daviddemeyer095@gmail.com>
2025-09-29 09:42:49 -0600

    Update functions.py

---
+++ src/goph419lab01/functions.py
@@ -0,0 +1,54 @@
+def sqrt(a):
+"""This will use five base point to generalize it, so any number from 0-0.75 will be 0.5 and then we set a sqrt number to this value, for example. 
+Parameters are (a) which represents the number we are finding the square root for from 0-2.5
+----------
+Returns y, which will be the square root of the number inputted
+-------
+"""
+input (a)
+if a<0 or a>2.5
+  print("please input number from 0-2.5")
+  quit()
+elif 0<=a<=0.75 
+  #a == 0.5 
+  y == 0.70710678
+elif 0.75<a<=1.25
+  #a == 1.0
+  y == 1.0
+elif 1.25<a<=1.75
+  #a == 1.5
+  y == 1.2247449
+elif 1.75<a<=2.25
+  #a == 2.0 
+  y == 1.4142136
+elif a=2.25<a<=2.5
+  #a == 2.5
+  y == 1.5811388 
+# implementation of positive square root
+# (using Taylor series)
+return y
+
+def arcsin(a):
+"""Description of function.
+Parameters
+----------
+Returns
+-------
+"""
+# implementation of arcsin
+# (using the series from
+# Borwein and Chamberlain 2007)
+return y
+
+def launch_angle_range(ve_v0, alpha, tol_alpha):
+"""Description of function.
+Parameters
+----------
+Returns
+-------
+"""
+# ...
+# your implementation here should call on your
+# functions implementing Equations (17) and (18)
+# ...
+return phi_range
