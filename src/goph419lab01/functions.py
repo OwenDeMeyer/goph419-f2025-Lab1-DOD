b3ea8b3742be20ecf4de8e020a637ab5496b2790
OwenDeMeyer <daviddemeyer095@gmail.com>
2025-09-30 15:50:58 -0600

    Update functions.py

--- src/goph419lab01/functions.py
+++ src/goph419lab01/functions.py
@@ -1,3 +1,6 @@
+def factorial(n):
+   return 1 if n == 0 else n * factorial(n - 1)
+
 def sqrt(a):
 """This will use five base point to generalize it, so any number from 0-0.75 will be 0.5 and then we set a sqrt number to this value, for example. 
 Parameters are (a) which represents the number we are finding the square root for from 0-2.5
@@ -7,38 +10,44 @@ Returns y, which will be the square root of the number inputted
 """
 input (a)
 if a<0 or a>2.5
-  print("please input number from 0-2.5")
-  quit()
-elif 0<=a<=0.75 
-  #a == 0.5 
-  y == 0.70710678
-elif 0.75<a<=1.25
-  #a == 1.0
-  y == 1.0
-elif 1.25<a<=1.75
-  #a == 1.5
-  y == 1.2247449
-elif 1.75<a<=2.25
-  #a == 2.0 
-  y == 1.4142136
-elif a=2.25<a<=2.5
-  #a == 2.5
-  y == 1.5811388 
+  raise ValueError('Input number from 0-2.5')
+if x ==0:
+  return 0.0
+if x ==0:
+  return 1.0
+
+ 
+term = a
+result = term
 # implementation of positive square root
 # (using Taylor series)
 return y
 
 def arcsin(a):
 """Description of function.
-Parameters
+Parameters Input number 'a' to compute arcsin for, must be between 0-1
 ----------
-Returns
+Returns the result after the while loop is completed
 -------
 """
+if a<0 or a>1:
+  raise ValueError('Input number from 0-1')
+
+term = a #first term
+result = term 
+old_result = 0 
+n = 1 
+
+cond = 1e-9 #condition for 8 significant figures
+
+while (result - old_result) > cond:
+  denom = (n**2) * (factorial(2*n) / ((factorial(n))**2)
+  term = ((2*a)**2n) / denom
+  old_result = result 
+  result += term
 # implementation of arcsin
-# (using the series from
-# Borwein and Chamberlain 2007)
-return y
+# (using the series from Borwein and Chamberlain 2007)
+return result
 
 def launch_angle_range(ve_v0, alpha, tol_alpha):
 """Description of function.
@@ -52,3 +61,4 @@ Returns
 # functions implementing Equations (17) and (18)
 # ...
 return phi_range
+
