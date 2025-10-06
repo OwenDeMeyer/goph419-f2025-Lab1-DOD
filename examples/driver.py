ac5690b5a8fcb954e92e9bbd5eefcc78c81010ed
OwenDeMeyer <daviddemeyer095@gmail.com>
2025-10-06 15:53:01 -0600

    Update driver.py

--- examples/driver.py
+++ examples/driver.py
@@ -1,4 +1,4 @@
-def main():
+def plot_alpha_vs_launch_angle():
     ve_v0 = 2.0
     tol_alpha = 0.04
 
@@ -33,5 +33,39 @@ def main():
     plt.savefig("figures/launch_angle_vs_alpha.png", dpi=300)
     plt.show()
 
-if __name__ == "__main__":
-    main()
+def plot_velocity_vs_launch_angle():
+    a = 0.25
+    tol_alpha = 0.04
+    ve_v0 = 0.5
+    ve_v0s = []
+    phi_min_deg = []
+    phi_max_deg = []
+
+    
+    while ve_v0 <= 6.0:
+        try:
+            phi_min, phi_max = launch_angle_range(ve_v0, a, tol_alpha)
+        except ValueError:
+            ve_v0 += 0.01
+            continue
+
+        ve_v0s.append(ve_v0)
+        phi_min_deg.append(phi_min * 180 / PI)
+        phi_max_deg.append(phi_max * 180 / PI)
+        
+        ve_v0 += 0.01
+
+    plt.figure(figsize=(7,5))
+    plt.plot(ve_v0s, phi_min_deg, label="Minimum launch angle", color="blue")
+    plt.plot(ve_v0s, phi_max_deg, label="Maximum launch angle", color="green")
+    plt.xlabel("Velocity Ratio (Ve/V0)")
+    plt.ylabel("Launch angle")
+    plt.title(f"Launch angle range vs Velocity Ratio ")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    os.makedirs("figures", exist_ok=True)
+    plt.savefig("figures/launch_angle_vs_alpha.png", dpi=300)
+    plt.show()
+
+
